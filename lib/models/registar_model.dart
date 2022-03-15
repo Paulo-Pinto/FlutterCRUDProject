8ae9d9014dcc2a441493230a248d5f5de299b7b1
Paulo-Pinto <paulopintodigital@gmail.com>
2022-03-15 00:25:54 +0000

    adicionei id ao Registo e função para mostrar info pertinente na lista.

--- lib/models/registar_model.dart
+++ lib/models/registar_model.dart
@@ -2,12 +2,11 @@ import 'package:intl/intl.dart';
 
 class RegistarModel {
   static RegistarModel? _instance;
+
   final _registos = [
     Registo(58, true, 4, "Vincent Aboubakar"),
     Registo(68, false, 1, "Moussa Marega")
   ];
-
-  // final registos = [];
 
   RegistarModel._internal();
 
@@ -16,7 +15,7 @@ class RegistarModel {
     return _instance;
   }
 
-  void insert(operation) => _registos.add(operation);
+  void insert(Registo registo) => _registos.add(registo);
 
   List getAll() => _registos;
 
@@ -28,25 +27,36 @@ class RegistarModel {
 }
 
 class Registo {
+  static int lastId = 0;
+  int id = lastId + 1;
   double peso = 0;
   bool comeu = false;
   int rate = 1;
   String obs = "";
   DateTime data = DateTime.now();
 
-  Registo(this.peso, this.comeu, this.rate, this.obs);
+  Registo(this.peso, this.comeu, this.rate, this.obs){
+    lastId++;
+  }
 
   @override
   String toString() {
-    return 'Registo{peso: $peso, comeu: $comeu, rate: $rate, obs: ' +
+    return 'Registo $id {peso: $peso, comeu: $comeu, rate: $rate, obs: ' +
         ((obs == "") ? "Nenhuma" : obs) +
         ', data: ' +
         formatDate() +
         '}';
   }
 
+  String toDisplay() {
+    return formatDate() +
+        " - Pesou ${peso} kg, " +
+        (!comeu ? " não " : "") +
+        "comeu, e avaliou o seu dia com $rate/5";
+  }
+
   String formatDate() {
-    final DateFormat formatter = DateFormat('dd-MM-yyyy H:mm');
+    final DateFormat formatter = DateFormat('dd/MM/yyyy - H:mm');
     // final String formatted = formatter.format(data);
     return formatter.format(data);
   }
